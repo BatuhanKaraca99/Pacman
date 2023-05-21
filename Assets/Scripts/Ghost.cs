ceed4badf9da8c10bf858b22f851661ef1b53bd7
Batuhan KARACA <b.karaca.1999@gmail.com>
2023-05-21 15:02:49 +0300

    Game Manager

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    public int points = 200; //we can change it,because it is public
+}
